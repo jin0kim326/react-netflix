599db44142d88808bc4a11fa0d496042c3144a1f
김진영 <53967482+jin0kim326@users.noreply.github.com>
2021-02-04 13:55:42 +0900

    header CSS & props control

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Router from "./components/router";
+import GlobalStyles from "./components/globalstyles";
 
 class App extends Component {
   render() {
     return (
       <>
         <Router />
+        <GlobalStyles />
       </>
     );
   }
